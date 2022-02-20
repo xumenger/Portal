0b0f1439a1c034c143ba5e040897a6a7f1597f6c
xumenger <xumenger@126.com>
2022-02-20 21:17:34 +0800

    Rpc结构定义

--- src/raft/RaftRpc.h
+++ src/raft/RaftRpc.h
@@ -2,16 +2,20 @@
  * 2022.02.20 by xumenger
  *
  * Raft RPC 通信数据结构定义
+ *
  */
+
 #ifndef PORTAL_RAFT_RPC_H
 #define PORTAL_RAFT_RPC_H
 
 #include <iostream>
+#include <set>
 
 #include "RaftNode.h"
 #include "RaftEntry.h"
 
 using namespace std;
+
 
 namespace portal
 {
@@ -48,10 +52,58 @@ namespace raft
     {
         string messageId;
         int term;
-        NodeId *leaderId;
+        NodeId *leaderId;                   // 指针？值？
         int prevLogIndex;
-    }
+    };
+    struct AppendEntriesResult
+    {
+        string rpcMessageId;
+        int term;
+        bool success;
+    };
+
+
+    struct InstallSnapshotRpc
+    {
+        int term;
+        NodeId *leaderId;
+        int lastIndex;
+        int lastTerm;
+        set<NodeEndpoint> lastConfig;       // 值类型？
+        int offset;
+        unsigned char *data;
+        bool done;
+    };
+    struct InstallSnapShotResult
+    {
+        int term;
+    };
+
+
+    enum GroupConfigChangeStatus { OK, TIMEOUT, NO_LEADER };
+
+    struct AddServerRpc
+    {
+        NodeEndpoint newServer;
+    };
+    struct AddServerResult
+    {
+        GroupConfigChangeStatus status;
+        NodeEndpoint leaderHint;
+    };
+
+    struct RemoveServerRpc
+    {
+        NodeEndpoint oldServer;
+    };
+    struct RemoveServerResult
+    {
+        GroupConfigChangeStatus status;
+        NodeEndpoint leaderHint;
+    };
+
 }
 }
 
 #endif
+
