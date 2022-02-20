f1e3d1f1b0c31a7ca94027aa619bc9efc3eae3eb
xumenger <xumenger@126.com>
2022-02-20 17:23:45 +0800

    日志

--- src/raft/RaftLog.h
+++ src/raft/RaftLog.h
@@ -7,6 +7,11 @@
 #ifndef PORTAL_RAFT_LOG_H
 #define PORTAL_RAFT_LOG_H
 
+#include <iostream>
+
+using namespace std;
+
+
 namespace portal
 {
 namespace raft
@@ -33,9 +38,58 @@ namespace raft
      */
     struct Entry
     {
-        EntryMeta meta;                 // 日志条目元信息
-        unsigned char *commandBytes;    // C++ 中没有byte 类型
+        public:
+            EntryMeta meta;                 // 日志条目元信息
+            unsigned char *commandBytes;    // C++ 中没有byte 类型
+
+            Entry(EntryKind kind, int index, int term)
+            {
+                this.meta.kind = kind;
+                this.meta.index = index;
+                this.meta.term = term;
+
+                this.commandBytes = NULL;
+            }
+
+            const EntryMeta &getEntryMeta()
+            {
+                return this.meta;
+            }
+
+            const unsigned char *getCommandBytes()
+            {
+                return this.commandBytes;
+            }
     };
+
+
+    /**
+     * No-Op 条目
+     */
+    struct NoOpEntry : public Entry
+    {
+        public:
+            NoOpEntry(int index, int term)
+                : Entry(KIND_NO_OP, index, term)
+            {
+
+            }
+    };
+
+    
+    /**
+     * 通用日志条目
+     */
+    struct GeneralEntry : public Entry
+    {
+        public:
+            GeneralEntry(int index, int term, unsigned char *commandBytes)
+                : Entry(KIND_GENERAL, index, term)
+            {
+                this.commandBytes = commandBytes;
+            }
+    };
+
 }
 }
 
