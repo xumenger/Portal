7a7bdaa30a12306cba6ef08bf3a56d0cb2ad28d9
xumenger <xumenger@126.com>
2022-02-20 23:16:49 +0800

    管理接口

---
+++ src/web/PortalBackend/src/main/java/com/xum/portal/PortalApplication.java
@@ -0,0 +1,19 @@
+package com.xum.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2022.02.20 by xumenger
+ * 
+ * Portal 框架的Web 管理后端
+ * 
+ */
+@SpringBootApplication 
+public class PortalApplication 
+{
+	public static void main(String[] args) 
+    {
+        SpringApplication.run(PortalApplication.class, args);
+    }
+}
---
+++ src/web/PortalBackend/src/main/java/com/xum/portal/controller/ManagerController.java
@@ -0,0 +1,12 @@
+package com.xum.portal.controller;
+
+/**
+ * 2022.02.20 by xumenger
+ * 
+ * Portal 框架的管理接口
+ * 
+ */
+public class ManagerController 
+{
+
+}
