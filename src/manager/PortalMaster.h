da2fdc4f9673232c115580b838a43f6070f5a631
xumenger <xumenger@126.com>
2022-02-20 22:35:07 +0800

    Manager

---
+++ src/manager/PortalMaster.h
@@ -0,0 +1,33 @@
+/**
+ * 2022.02.20 by xumenger
+ *
+ * Portal 分布式框架的Master 组件
+ * 负责管理集群中的所有工作节点
+ *
+ */
+#ifndef PORTAL_MASTER_H
+#define PORTAL_MASTER_H
+
+#include <iostream>
+
+using namespace std;
+
+namespace portal
+{
+namespace master
+{
+    // 集群中的工作节点信息
+    class PortalWorker
+    {
+
+    };
+
+    // 集群中的管理节点信息
+    class PortalMaster
+    {
+
+    };
+}
+}
+
+#endif
