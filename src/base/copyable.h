02af1d5fbd3dec4f430f1ce394395651d5d97c3c
xumenger <xumenger@126.com>
2022-03-13 22:24:50 +0800

    TCP基础类

---
+++ src/base/copyable.h
@@ -0,0 +1,26 @@
+/**
+ * 2022.03.13
+ *
+ * 模仿boost::noncopyable
+ * 用于标记某个类具有值语义，是可以拷贝的
+ *
+ */
+#ifndef PORTAL_BASE_COPYABLE_H
+#define PORTAL_BASE_COPYABLE_H
+
+namespace portal
+{
+namespace base
+{
+
+    
+class copyable
+{
+};
+
+
+};
+};
+
+
+#endif;
