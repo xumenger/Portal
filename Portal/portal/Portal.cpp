57ea9e9cd426d7d297796e373cbbca930afec92f
xumenger <xumenger@126.com>
2024-02-20 23:50:19 +0800

    Portal

--- Portal/portal/Portal.cpp
+++ Portal/portal/Portal.cpp
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cerrno>
+
+#define PORTAL_PORT 7777  // 端口号
+#define BUF_SIZE 1024     // 最大缓存
+#define MAX_QUEUE 20      // 最大连接数 
 
 /**
  * Portal分布式数据节点
@@ -24,6 +34,58 @@
  */
 int main(int argc, char const *argv[])
 {
+    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_sockaddr;                 // 保存本地地址信息
+    server_sockaddr.sin_family = AF_INET;               // 采用ipv4
+    server_sockaddr.sin_port = htons(PORTAL_PORT);      // 指定端口
+    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);// 获取主机接收的所有响应
+
+    // 绑定本地IP与端口
+    if (bind(server_sockfd, (struct sockaddr *)&server_sockaddr, sizeof(server_sockaddr)) == -1) {
+        perror("Bind Failuer\n");
+        printf("Error: %s\n", strerror(errno));     // 输出错误信息
+        return -1;
+    }
+
+    // 设置监听状态
+    printf ("Listen Port: %d\n", PORTAL_PORT);
+    if (listen(server_sockfd, MAX_QUEUE) == -1) {
+        perror("Listen Error\n");
+        return -1;
+    }
+
+    char buffer[BUF_SIZE];                      // 一次传输的数据缓存
+    struct sockaddr_in client_addr;             // 保存客户端地址信息
+    socklen_t length = sizeof(client_addr);     // 需要的内存大小
+
+    printf("Waiting for connection!\n");
+
+    // 等待连接，返回服务器端建立连接的socket
+    int connect_fd = accept(server_sockfd, (struct sockaddr *)&client_addr, &length);
+    if (-1 == connect_fd) {
+        perror("Connect Error\n");
+        return -1;
+    }
+
+    printf("Connection Successful\n");
+
+    // 数据收发与传输
+    while(1) {
+        memset(buffer, 0, sizeof(buffer));
+        int len = recv(connect_fd, buffer, sizeof(buffer), 0);
+        if (strcmp(buffer, "exit\n") == 0 || len <= 0)
+            break;
+        printf("client send message: %s", buffer);
+        strcpy(buffer, "successful");
+        send(connect_fd, buffer, strlen(buffer), 0);
+        printf("send message: %s\n", buffer);
+    }
+
+    close(connect_fd);
+    close(server_sockfd);
+
+
 	// 启动与客户端通信的Loop
 	// 涉及到哪些网络接口？
 
