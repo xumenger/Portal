b097237bc94c5b5740a7e9efe8f226f910e58ce0
xumenger <xumenger@126.com>
2024-02-21 00:24:18 +0800

    管理接口

---
+++ Transfer/src/main/java/com/xum/controller/ManageController.java
@@ -0,0 +1,10 @@
+package com.xum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manage")
+public class ManageController {
+
+}
