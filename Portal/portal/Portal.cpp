c5393f6b8ad52687b9a6ad8551dbbf2d922b5fad
xumenger <xumenger@126.com>
2024-02-25 19:46:10 +0800

    epoll LT

--- Portal/portal/Portal.cpp
+++ Portal/portal/Portal.cpp
@@ -1,9 +1,13 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <cerrno>
 
 #include <iostream>
@@ -11,9 +15,15 @@
 #include "../protobuf/Portal.pb.h"
 
 
-#define PORTAL_PORT 7777  // 端口号
-#define BUF_SIZE 1024     // 最大缓存
-#define MAX_QUEUE 20      // 最大连接数 
+#define PORTAL_PORT 7777        // 端口号
+#define BUF_SIZE 1024           // 最大缓存
+#define BUFFER_SIZE 10
+#define MAX_QUEUE 20            // 最大连接数
+#define MAX_EVENT_NUMBER 1024   // epoll最大事件数
+
+int setnonblocking(int fd);
+void addfd(int epollfd, int fd);
+void lt(epoll_event *events, int number, int epollfd, int listenfd);
 
 /**
  * Portal分布式数据节点
@@ -38,6 +48,103 @@
  * 
  */
 int main(int argc, char const *argv[])
+{
+    if (argc <= 2) {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        return 1;
+    }
+
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+    
+    int ret = 0;
+    struct sockadd_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+
+    ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(listenfd, MAX_QUEUE);
+    assert(ret != -1);
+
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);  // 参数5是什么？
+    assert(epollfd != -1);
+    addfd(epollfd, listenfd);       // listenfd是要用ET模式还是LT模式
+
+    while(1) {
+        int ret = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (ret < 0) {
+            printf("epoll failure\n");
+            break;
+        }
+
+        // ret返回值指的是？
+        lt(events, ret, epollfd, listenfd);
+    }
+
+    close(listenfd);
+    return 0;
+}
+
+// 将文件描述符设置为非阻塞的
+int setnonblocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+// 将文件描述符上的EPOLLIN 注册到epollfd 指示的epoll内核事件表，使用LT模式（默认）
+void addfd(int epollfd, int fd)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+// lt模式的工作流
+void lt(epoll_event *events, int number, int epollfd, int listenfd)
+{
+    char buf[BUFFER_SIZE];
+    for (int i=0; i<number; i++) {
+        int sockfd = events[i].data.fd;
+        if (sockfd == listenfd) {
+            struct sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof(client_address);
+            int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlength);
+            addfd(epollfd, connfd);
+        }
+        else if (events[i].event & EPOLLIN) {
+            // 只要socket度缓存中还有未读出的数据，这段代码就会被触发
+            memset(buf, '\0', BUFFER_SIZE);
+            // 返回值分别有哪些可能？
+            int ret = recv(sockfd, buf, BUFFER_SIZE-1, 0);
+            if (ret <= 0) {
+                close(sockfd);
+                continue;
+            }
+        }
+        else {
+            printf("something else happened \n");
+        }
+    }
+}
+
+
+
+
+int backup(int argc, char const *argv[])
 {
     int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
