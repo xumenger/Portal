616c60656cf90eddcb1f30ca05d48210f0bd1408
xumenger <xumenger@126.com>
2024-03-03 22:24:04 +0800

    first / second

--- Portal/agent/Agent.cpp
+++ Portal/agent/Agent.cpp
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <assert.h>
@@ -54,17 +55,16 @@ int main(int argc, char const *argv[])
        printf("connection failed\n");
     }
     else {
-        com::xum::proto::portal::SetRequest set_req;
-        set_req.set_key("key");
-        set_req.set_value("value");
+        com::xum::proto::portal::GetRequest get_req;
+        get_req.set_key("CreateWorker");
        
-        int size = set_req.ByteSize(); 
+        int size = get_req.ByteSize(); 
         char ss[size];
-        set_req.SerializeToArray(ss, size);
+        get_req.SerializeToArray(ss, size);
 
         
         char int_buffer[sizeof(int32_t)];
-        int32_t msg_type = htonl(com::xum::proto::portal::MsgSetReq);
+        int32_t msg_type = htonl(com::xum::proto::portal::MsgGetReq);
         memcpy(&int_buffer, &msg_type, sizeof(msg_type));
         send(sockfd, int_buffer, 4, 0);
 
@@ -72,9 +72,15 @@ int main(int argc, char const *argv[])
         memcpy(&int_buffer, &msg_len, sizeof(msg_len));
         send(sockfd, int_buffer, 4, 0);
 
+        // 暂不严格考虑发送失败的情况
         if (send(sockfd, ss, size, 0) <= 0) {
             printf("send error\n");
         }
+
+        // 接收应答（暂时只考虑阻塞的方式）
+        get_response(connect_fd);
+
+        fork_worker(com::xum::proto::portal::GetResponse get_rsp);
 
         // 接收Portal的调度指令：
         // 1. 启动Worker
@@ -95,4 +101,46 @@ int main(int argc, char const *argv[])
     // 支持lua方式fork多线程进程
 
     return 0;
+}
+
+
+void get_response(int connect_fd) {
+    char int_buffer[sizeof(int32_t)];
+
+    int32_t msg_type_temp;
+    int len = recv(connect_fd, &int_buffer, 4, 0);
+    msg_type_temp = *((int32_t*)int_buffer);
+    msg_type_temp = ntohl(msg_type_temp);
+    // 在c中，enum类型默认是int类型，它们之间可以自动转换
+    // c++编译器支持从enum类型自动转换为int，但反过来是不支持的。需要进行强制转换
+    com::xum::proto::portal::PortalMessageType type = com::xum::proto::portal::PortalMessageType(msg_type_temp);
+
+    int32_t msg_len;
+    len = recv(connect_fd, &int_buffer, 4, 0);
+    msg_len = *((int32_t*)int_buffer);
+    msg_len = ntohl(msg_len);
+
+    char buffer[msg_len];
+    len = recv(connect_fd, buffer, msg_len, 0);
+
+    com::xum::proto::portal::GetResponse get_rsp;
+    get_rsp.ParseFromArray(buffer, msg_len);
+
+    std::cout << get_rsp.value() << std::endl;
+}
+
+
+// TODO 怎么制作一个后台进程？
+void fork_worker(com::xum::proto::portal::GetResponse get_rsp) {
+    // 暂不考虑解析请求指令的问题
+
+    // 直接fork 一个进程
+    int childpid = fork();
+    
+    if(childpid == 0){
+        printf("%d This is child process\n",childpid);
+    }else{
+        printf("This is parent process\n");}
+    }
+    printf("%d step2 after fork()！！ \n\n",childpid);
 }
